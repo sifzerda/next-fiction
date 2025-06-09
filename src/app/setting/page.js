09ee1e97c691b07a3e50b328342d1d5804dccea5
Troygit config --global user.email <tydamon@hotmail.comgit config --global user.nme>
2025-06-09 14:53:27 +1000

    fixed setting page formatting

--- src/app/setting/page.js
+++ src/app/setting/page.js
@@ -1,10 +1,10 @@
-// src/app/setting.js
+// src/app/secondaryCharacters.js
 "use client";
 import Layout from '../../components/Layout';
 import CommentForm from '../../components/CommentForm';
 import CommentList from '../../components/CommentList';
 import FootnoteRef from '../../components/FootnoteRef';
-import settingBanner from '../assets/settingbanner.jpg'; // Adjust the path as necessary
+import settingBanner from '../assets/settingbanner.jpg';
 import Image from "next/image";
 
 function Setting() {
@@ -42,29 +42,37 @@ function Setting() {
 
           <h3 id="post-enhance-interest-and-conflict">9.2: Enhance Interest and Conflict</h3>
           <p>It's a mistake to see setting as a passive element of a story, like the 'virtual wallpaper' that papers the background of the action. Approaching setting as an afterthought, by uncritical intuition, or coin-flipping between alternatives, or wanting to vicariously 'visit' the scene for personal reasons, are all great ways to <i>not</i> select your setting.</p>
+
           <p>Some basic rules:</p>
 
-          <ul>
+          <ol>
             <li>Like characters and dialogue, story settings are not real places. They are patchworks of details and virtual ‘set pieces’ and ‘props’ calculated to enhance conflict and interest in the story;</li>
-            <ul>
-              <li>Another way of thinking of it is like lego; you can break up and add to any part of your scene how you need to for the story. Readers will forgive a strange setting for particularisation setting it apart from other scenes in other stories; e.g.</li>
-              <ul>
-                <li>A haunted mansion that is half bulldozed (meaning you can walk into a room leading directly to outside);</li>
-                <li>An amusement park in the middle of the forest;</li>
-              </ul>
-              <li>Another way to particularise or add new life to settings is to make the characters behave unexpectedly or unconventionally in them. The movie ‘The Terminal’ (starring Tom Hanks) is a great example of this, with a man living in an airport;</li>
-            </ul>
+          </ol>
+          <ol>
+            <li>Another way of thinking of it is like lego; you can break up and add to any part of your scene how you need to for the story. Readers will forgive a strange setting for particularisation setting it apart from other scenes in other stories; e.g.</li>
+          </ol>
+          <ol>
+            <li>A haunted mansion that is half bulldozed (meaning you can walk into a room leading directly to outside);</li>
+          </ol>
+          <ol>
+            <li>An amusement park in the middle of the forest;</li>
+          </ol>
+          <ol>
+            <li>Another way to particularise or add new life to settings is to make the characters behave unexpectedly or unconventionally in them. The movie ‘The Terminal’ (starring Tom Hanks) is a great example of this, with a man living in an airport;</li>
+          </ol>
+          <ol>
             <li>Think of a setting less like a location in a movie, and more like a set on a theatre stage: props and scenery flats economically selected to suggest a broader place; a door suggesting a room, a child’s toy suggesting a play pen, a beach ball and floatie suggesting a pool, etc. Your setting isn’t a pretty backdrop (after all, unlike in a movie, your reader won’t be able to see it) it’s a playground of ‘toys’ for your characters to interact with; with ‘let mot juste’ rule in mind, you only detail your setting with the relevant toys and details to make the story move. Your reader will always assume anything obvious which wasn’t described;</li>
+          </ol>
+          <ol>
             <li>Scenes should provide obstacle to the protagonist, not provide obstacle to the story.</li>
-          </ul>
+          </ol>
 
           <h3 id="post-describing-setting">9.3: Describing Setting</h3>
+
           <p>Physical objects should be given physical descriptions. </p>
 
           <div className="p-3 mb-3 rounded bg-gray-100 border border-gray-300 font-rubik">
-            <p> When we introduce a thing in a novel, the reader must be given a clear picture of it [...] When a place is introduced in a [story], the reader must see it with sufficient clarity to be able to form a mental image of it."
-              <FootnoteRef text="Robert Meredith and John Fitzgerald, Structuring Your Novel: From Basic Idea to Finished Manuscript (Everyday Handbook)." />
-            </p>
+            <p>When we introduce a thing in a novel, the reader must be given a clear picture of it [...] When a place is introduced in a [story], the reader must see it with sufficient clarity to be able to form a mental image of it." <FootnoteRef text="Robert Meredith and John Fitzgerald, Structuring Your Novel: From Basic Idea to Finished Manuscript (Everyday Handbook)." /></p>
           </div>
 
           <p>Descriptions of settings should be concrete, not emotional (i.e. how the place makes the character feel). Objective observations based on the senses are fine, however (e.g. 'hot' or 'silent'). In other words, characters should be able to agree on the observation.</p>
@@ -78,27 +86,38 @@ function Setting() {
           <h3 id="post-setting-as-character">9.4: Setting as Character</h3>
 
           <p>Your setting isn’t just a landscape or backdrop; it can also be a character. Use your environment to suggest things which cannot be said in dialogue or description. Examples: </p>
-
-          <ul>
+          <ol>
             <li>A character having to ‘climb stairs’ to get somewhere suggests the activity at the forthcoming place will be difficult or tiresome, and is not something looked forward to by the character;</li>
             <li>A thunderstorm during a party or get-together suggests underlying tensions between guests which might ignite;</li>
             <li>A house with furniture that is incredibly heavy or unmovable suggests the occupants are settled in for the long term, or maybe feel trapped there against their own preferences;</li>
             <li>Transport can be a setting, too – e.g. the inside of a car, train, houseboat, elevator, etc. Settings in motion can add an extra dimension of action and uncertainty because characters are only together for a finite period of time – the duration of the trip;</li>
-          </ul>
+          </ol>
 
           <p>Example from Pet Sematary:</p>
-          <p className="italic">Then there was a sound, and he remembered this from the last time as well: a high, gobbling laugh that became a sob. There was silence for a moment and then the laugh came again, this time rising to a maniacal shriek that froze Louis’s blood. The mist drifted dreamily around him.</p>
+
+          <p className="italic text-midBluey">Then there was a sound, and he remembered this from the last time as well: a high, gobbling laugh that became a sob. There was silence for a moment and then the laugh came again, this time rising to a maniacal shriek that froze Louis’s blood. The mist drifted dreamily around him.</p>
+
           <p>Jumping from Louis's reaction (his blood froze) to the setting (the mist) creates an unspoken dialogue between protagonist and setting. The protagonist asks a question "what made that sound?" and the setting answers "I'll tell you nothing". See this example, rewritten to explicitly provide the same information:</p>
-          <p className="italic">[...S]ilence for a moment and then the laugh came again, this time rising to a maniacal shriek that froze Louis’s blood. <u>Unable to move, Louis could only stare about into the forest for what had made that surreal sound. But he saw nothing.</u> The mist drifted dreamily around him.</p>
+
+          <p className="italic text-midBluey">[...S]ilence for a moment and then the laugh came again, this time rising to a maniacal shriek that froze Louis’s blood. <u>Unable to move, Louis could only stare about into the forest for what had made that surreal sound. But he saw nothing.</u> The mist drifted dreamily around him.</p>
+
           <p>The underlined text shows what is already contained in the source by the immediate cut to the setting (the mist), and keeps us in the scene in real time, without removing us from the scene and putting us inside the protagonist's head.</p>
 
-          <h3 id="post-sub-setting">9.5: Sub-Setting</h3>
+          <h3 id="post-sub-setting">9.5: Sub-Setting </h3>
+
           <p>You don't have to change the location to get a different setting. Just change some details. Change the time of day (or night) or weather, or move in time to action a setting change.</p>
           <p>Note that changing a setting can have a different effect than switching to a new setting. A setting that performs the exact same function despite looking different, can be reassuring and provide a sense of stability and routine, or a return to order. Conversely, a formerly familiar setting that now appears different  or foreign can represent uncertainty and be unsettling. </p>
           <p>Otherwise, if the setting hasn't changed, it may provide a foil to emphasise how much the character has changed by contrast.</p>
 
-          <h3 id="post-setting-is-relative">9.6: Setting is Relative</h3>
-          <p>Setting can be transformed as the character is. If a character is very small (e.g. Stuart Little<FootnoteRef text="Stuart Little, E. B. White." />) a single setting might be a creek or part of a room. If a character is very large, (e.g. Gulliver's Travels<FootnoteRef text="Gulliver's Travels, Jonathan Swift." />) a setting might be an entire city. Setting can also be an entire time period, e.g. the Late Cretaceous period Age in A Sound of Thunder by Ray Bradbury.<FootnoteRef text="Ray Bradbury, The Golden Apples of the Sun." /></p>
+          <h3 id="post-setting-is-relative">9.6: Setting is Relative </h3>
+
+          <p>Setting can be transformed as the character is. If a character is very small (e.g. Stuart Little
+            <FootnoteRef text="Stuart Little, E. B. White." />
+            ) a single setting might be a creek or part of a room. If a character is very large, (e.g. Gulliver's Travels
+            <FootnoteRef text="Gulliver's Travels, Jonathan Swift." />
+            ) a setting might be an entire city. Setting can also be an entire time period, e.g. the Late Cretaceous period Age in A Sound of Thunder by Ray Bradbury.
+            <FootnoteRef text="Ray Bradbury, The Golden Apples of the Sun." />
+          </p>
 
           {/* ------------------------------------------------- Thoughts Form footnotes --------------------------------------------- */}
 
