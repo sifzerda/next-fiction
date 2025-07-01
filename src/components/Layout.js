3871200a4f481091cecf3b3f77b7e6743c93d3f9
Troygit config --global user.email <tydamon@hotmail.comgit config --global user.nme>
2025-07-01 14:11:57 +1000

    reduced main pb

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,7 @@ function Layout({ children }) {
         <aside className="w-full bg-gray-100 dark:bg-gray-900 sm:bg-transparent sm:dark:bg-transparent sm:w-56 p-6">
           <Sidebar />
         </aside>
-        <main className="flex-1 p-4 sm:px-6 flex flex-col gap-8 items-center sm:items-start pb-28">
+        <main className="flex-1 p-4 sm:px-6 flex flex-col gap-8 items-center sm:items-start pb-16">
           {children}
         </main>
       </div>
