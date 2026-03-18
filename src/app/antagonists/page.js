98be7885710271a500793646b9082d9e6df5c65c
Troygit config --global user.email <tydamon@hotmail.com>
2026-03-18 20:17:22 +1100

    updated2

--- src/app/antagonists/page.js
+++ src/app/antagonists/page.js
@@ -4,7 +4,7 @@ import Layout from '../../components/Layout';
 import CommentForm from '../../components/CommentForm';
 import CommentList from '../../components/CommentList';
 import FootnoteRef from '../../components/FootnoteRef';
-import antagBanner from '../assets/antagbanner.jpg';
+import antagBanner from '../assets/antagBanner.jpg';
 import Image from "next/image";
 
 function Antagonists() {
