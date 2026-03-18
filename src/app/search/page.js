2b53cde361ee8e6e82ebc17a9fb2cabe3116ee8b
Troygit config --global user.email <tydamon@hotmail.com>
2026-03-18 22:43:01 +1100

    19

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,8 +1,14 @@
 // /app/search/page.js
 'use client';
-
+import { Suspense } from 'react';
 import SearchResults from './results';
 
+export const dynamic = 'force-dynamic'; // Prevents static prerendering
+
 export default function Page() {
-  return <SearchResults />;
+  return (
+    <Suspense fallback="Loading...">
+      <SearchResults />
+    </Suspense>
+  );
 }
